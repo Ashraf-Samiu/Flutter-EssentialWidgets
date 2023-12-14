f5752ed8637e0207f3275a58e40eee756038785b
ashraf <ashrafhossainsamiu@gmail.com>
2023-12-14 20:56:12 +0600

    MaterialApp commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-///AppBar
+///MaterialApp
 void main(){
   runApp(const MyApp());
 }
@@ -7,37 +7,25 @@ class MyApp extends StatelessWidget{
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      darkTheme: ThemeData(primarySwatch: Colors.amber),
       debugShowCheckedModeBanner: false,
-      home: HomeActivity(),
+      color: Colors.deepOrange,
+      title: "Application Title",
+      home: const HomeActivity(),
     );
   }
 }
 class HomeActivity extends StatelessWidget{
   const HomeActivity({super.key});
-  mySnackBar(messege,context){
-    return ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(messege))
-    );
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Test Application"),
-        titleSpacing: 50,
-        centerTitle: true,
-        toolbarHeight: 60,
-        toolbarOpacity: 0.7,
-        elevation: 10,
-        actions: [
-          IconButton(onPressed: () {mySnackBar("this is messeneger", context);},icon: Icon(Icons.message)),
-          IconButton(onPressed: (){mySnackBar("this is search", context);}, icon: Icon(Icons.search)),
-          IconButton(onPressed: (){mySnackBar("this is comment ", context);}, icon: Icon(Icons.comment)),
-          IconButton(onPressed: (){mySnackBar("This is settings", context);}, icon: Icon(Icons.settings)),
-          IconButton(onPressed: (){mySnackBar("This is profile", context);}, icon: Icon(Icons.person))
-        ],
+        title: const Text("Test Application"),
       ),
+      body: const Text("Hello World!"),
     );
   }
 
