706a204cbb3ba638a23b380accc174f9a9cd9b48
ashraf <ashrafhossainsamiu@gmail.com>
2023-12-14 21:10:50 +0600

    FloatingActionButton commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-///SnackBarMessage
+///FloatingActionButton
 void main(){
   runApp(const MyApp());
 }
@@ -7,13 +7,10 @@ class MyApp extends StatelessWidget{
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.green),
-      darkTheme: ThemeData(primarySwatch: Colors.amber),
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      color: Colors.deepOrange,
       title: "Application Title",
-      home: const HomeActivity(),
+      home: HomeActivity(),
     );
   }
 }
@@ -29,11 +26,14 @@ class HomeActivity extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(
         title: const Text("Test Application"),
-        actions: [
-          IconButton(onPressed: (){mySnackBar("This is label", context);}, icon: const Icon(Icons.label))
-        ],
       ),
-      body: const Text("Hello World!"),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red,
+        elevation: 10,
+          child: const Icon(Icons.add),
+          onPressed: (){
+            mySnackBar("This is a Floating Example", context);
+          }),
     );
   }
 
