ab047aece744624b7ef812eaa9a731f1542e4c7b
ashraf <ashrafhossainsamiu@gmail.com>
2023-12-14 20:41:57 +0600

    AppBar commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+///flutter project structure
 void main(){
   runApp(const MyApp());
 }
@@ -14,14 +14,29 @@ class MyApp extends StatelessWidget{
 }
 class HomeActivity extends StatelessWidget{
   const HomeActivity({super.key});
-
+  mySnackBar(messege,context){
+    return ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(messege))
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("First Application")
+        title: Text("Test Application"),
+        titleSpacing: 50,
+        centerTitle: true,
+        toolbarHeight: 60,
+        toolbarOpacity: 0.7,
+        elevation: 10,
+        actions: [
+          IconButton(onPressed: () {mySnackBar("this is messeneger", context);},icon: Icon(Icons.message)),
+          IconButton(onPressed: (){mySnackBar("this is search", context);}, icon: Icon(Icons.search)),
+          IconButton(onPressed: (){mySnackBar("this is comment ", context);}, icon: Icon(Icons.comment)),
+          IconButton(onPressed: (){mySnackBar("This is settings", context);}, icon: Icon(Icons.settings)),
+          IconButton(onPressed: (){mySnackBar("This is profile", context);}, icon: Icon(Icons.person))
+        ],
       ),
-      body: const Text("Hello World!"),
     );
   }
 
