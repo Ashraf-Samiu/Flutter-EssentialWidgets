a2fd8f19a12eb19d559b3f440708d9f64ea9ed92
ashraf <ashrafhossainsamiu@gmail.com>
2023-12-17 15:40:10 +0600

    RichText commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,22 +20,47 @@ class HomeScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text("Profile"),
       ),
-      body: Container(
-        height: 300,
-        width: 250,
-        child: Text("Hello Coders"),
-        alignment: Alignment.center,
-        margin: EdgeInsets.fromLTRB(10, 20, 30, 40),
-        padding: EdgeInsets.all(50),
-        decoration: BoxDecoration(
-          color: Colors.blue,
-          border: Border.all(
-            color: Colors.black,
-            width: 5
-          ),
-        ),
+      body: Column(
+        children: [
+          RichText(text: TextSpan(
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.blue,
+            ),
+            children: [
+              TextSpan(
+                text: "Hello ",
+              ),
+              TextSpan(
+                text: "Programmer ",
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+              ),
+              TextSpan(
+                text: "Write Clean Code!",
+                style: TextStyle(
+                  color: Colors.amberAccent,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800
+                )
+              ),
+            ]
+          )),
+          RichText(text: TextSpan(
+            text: "To Register, Tap On ",
+            children: [
+              TextSpan(
+                text: "Sign Up",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20
+                )
+              )
+            ]
+          ))
+        ],
       ),
     );
   }
-
 }
