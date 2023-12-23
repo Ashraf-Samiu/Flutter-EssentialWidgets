4b1fe42fe3c756537e6098aad8760710ff2540c7
ashraf <ashrafhossainsamiu@gmail.com>
2023-12-23 08:44:23 +0600

    TabBar Commit

---
+++ lib/Fragment_Screen/ac_unit.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Ac_unit extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("This is ac_unit!"),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/Fragment_Screen/ac_unit.dart';
+import 'package:flutter_widgets/Fragment_Screen/balance.dart';
+import 'package:flutter_widgets/Fragment_Screen/bussiness.dart';
+import 'package:flutter_widgets/Fragment_Screen/email.dart';
+import 'package:flutter_widgets/Fragment_Screen/home.dart';
+import 'package:flutter_widgets/Fragment_Screen/profile.dart';
+import 'package:flutter_widgets/Fragment_Screen/search.dart';
+import 'package:flutter_widgets/Fragment_Screen/settings.dart';
 
 void main(){
   runApp(MyApp());
@@ -13,49 +21,61 @@ class MyApp extends StatelessWidget{
   }
 }
 class HomeScreen extends StatelessWidget{
-  mySnackBar(context,message){
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text(message))
-    );
-  }
-  ///Json Array
-  var myItems= [
-    {"img": "https://cdn.hashnode.com/res/hashnode/image/upload/v1679571998369/3d121736-2637-43b7-ac55-d4370e57770d.png","title": "Ashraf"},
-    {"img": "https://cdn.hashnode.com/res/hashnode/image/upload/v1679571998369/3d121736-2637-43b7-ac55-d4370e57770d.png","title": "Karim"},
-    {"img": "https://cdn.hashnode.com/res/hashnode/image/upload/v1679571998369/3d121736-2637-43b7-ac55-d4370e57770d.png","title": "Rafiq"},
-    {"img": "https://cdn.hashnode.com/res/hashnode/image/upload/v1679571998369/3d121736-2637-43b7-ac55-d4370e57770d.png","title": "Shafiq"},
-    {"img": "https://cdn.hashnode.com/res/hashnode/image/upload/v1679571998369/3d121736-2637-43b7-ac55-d4370e57770d.png","title": "Sakib"}
-  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-      ),
-      body: GridView.builder(
-          gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 5,
-              childAspectRatio: 1.2
-          ),
-          itemCount: myItems.length,
-          itemBuilder: (context,index){
-            return GestureDetector(
-              onTap: (){
-                mySnackBar(context, myItems[index]["title"]);
-              },
-              child: Container(
-                margin: EdgeInsets.all(6),
-                width: double.infinity,
-                height: 200,
-                child: Image.network(
-                  myItems[index]["img"]!,
-                  fit: BoxFit.fill,
-                ),
+    return DefaultTabController(
+        length: 8,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("Home"),
+              bottom: TabBar(
+                isScrollable: true,
+                  tabs: [
+                    Tab(
+                      icon: Icon(Icons.home),
+                      text: "Home",
+                    ),
+                    Tab(
+                      icon: Icon(Icons.account_circle),
+                      text: "Profile",
+                    ),
+                    Tab(
+                      icon: Icon(Icons.settings),
+                      text: "Settings",
+                    ),
+                    Tab(
+                      icon: Icon(Icons.add_business),
+                      text: "Bussiness",
+                    ),
+                    Tab(
+                      icon: Icon(Icons.search),
+                      text: "Search",
+                    ),
+                    Tab(
+                      icon: Icon(Icons.email),
+                      text: "Email",
+                    ),
+                    Tab(
+                      icon: Icon(Icons.ac_unit),
+                      text: "Ac-Unit",
+                    ),
+                    Tab(
+                      icon: Icon(Icons.balance),
+                      text: "balance",
+                    )
+                  ]
               ),
-            );
-          }),
-    );
+            ),
+            body: TabBarView(children: [
+              Home(),
+              Profile(),
+              Settings(),
+              Bussiness(),
+              Search(),
+              Email(),
+              Ac_unit(),
+              Balance()
+            ]),
+        ));
   }
 }
