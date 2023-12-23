03fc54b599fdd62e2d4e54ceca815cc8f0f99b61
ashraf <ashrafhossainsamiu@gmail.com>
2023-12-23 09:39:38 +0600

    CirCularProgressIndicator Commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,20 +20,10 @@ class HomeScreen extends StatelessWidget{
         title: Text("Card"),
       ),
       body: Center(
-        child: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30)
-          ),
-          shadowColor: Color.fromRGBO(21, 23, 21, 1),
-          color: Color.fromARGB(1000, 200, 71, 100),
-          elevation: 50,
-          child: SizedBox(
-            height: 200,
-            width: 200,
-            child: Center(
-              child: Text("This is Card!"),
-            ),
-          ),
+        child: CircularProgressIndicator(
+          color: Colors.blue,
+          strokeWidth: 10,
+          backgroundColor: Colors.green,
         ),
       ),
     );
