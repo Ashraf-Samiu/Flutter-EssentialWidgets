77063db389950389cc3171c8b541fe59c6123d5f
ashraf <ashrafhossainsamiu@gmail.com>
2023-12-27 05:39:58 +0600

    BottomSheetDialog_AlertDialog Commit

--- lib/main.dart
+++ lib/main.dart
@@ -16,37 +16,45 @@ class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple,
       appBar: AppBar(
-        backgroundColor: Colors.yellow,
         centerTitle: true,
-        title: Text("Home",
-          style: TextStyle(
-            color: Colors.black54,
-          ),
-        ),
-        leading: Icon(
-          Icons.abc_rounded,
-          color: Colors.blue,
-          size: 30,
-        ),
+        title: Text("Home",),
       ),
       body: Center(
         child: Column(
           children: [
-            Image.network(
-                "https://t4.ftcdn.net/jpg/02/44/43/69/360_F_244436923_vkMe10KKKiw5bjhZeRDT05moxWcPpdmb.jpg",
-                width: 200,
-                height: 300,
-                fit: BoxFit.scaleDown,
-                repeat: ImageRepeat.repeatY,///will work only when it is in scaleDown fit
+            ElevatedButton(
+              onPressed: () {
+                showModalBottomSheet(
+                    isDismissible: false,
+                    backgroundColor: Colors.yellow,
+                    barrierColor: Colors.pink.shade300,
+                    context: context,
+                    builder: (_) {
+                      return Column(
+                        children: [
+                          Text('Message'),
+                          Text('Hello world'),
+                        ],
+                      );
+                    });
+              },child: Text('Tap here'),
             ),
-            Image.asset(
-              "images/234.jpg",
-              height: 300,
-              width: 400,
-              fit: BoxFit.cover,
-            )
+            ElevatedButton(
+                onPressed: (){
+                    showAboutDialog(context: context,);
+                    showDialog(context: context, builder: (context) {
+                     return AlertDialog(
+                        title: Text('Message'),
+                        content: Text('Hello, welcome to app'),
+                        actions: [
+                         ElevatedButton(onPressed: () {},
+                           child: Text('cancel'),
+                         ),
+                      ],
+                     );
+                   });
+                }, child: Text("Click Here!"))
           ],
         ),
       ),
