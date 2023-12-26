a31d986de43e7c41a92a866fc56238a50a7a64e5
ashraf <ashrafhossainsamiu@gmail.com>
2023-12-27 05:17:59 +0600

    Cart_With_GridView Commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,27 +18,17 @@ class HomeScreen extends StatelessWidget{
       SnackBar(content: Text(message))
     );
   }
-  var myItems = [
-    {"Name": "Ashraf", "Age": "20", "Address": "Dhaka City", "Country": "Bangladesh"},
-    {"Name": "John", "Age": "25", "Address": "New York", "Country": "USA"},
-    {"Name": "Emma", "Age": "22", "Address": "London", "Country": "UK"},
-    {"Name": "Carlos", "Age": "30", "Address": "Madrid", "Country": "Spain"},
-    {"Name": "Yuki", "Age": "28", "Address": "Tokyo", "Country": "Japan"},
-    {"Name": "Mia", "Age": "24", "Address": "Sydney", "Country": "Australia"},
-    {"Name": "Pablo", "Age": "26", "Address": "Buenos Aires", "Country": "Argentina"},
-    {"Name": "Lily", "Age": "23", "Address": "Paris", "Country": "France"},
-    {"Name": "Ravi", "Age": "27", "Address": "Mumbai", "Country": "India"},
-    {"Name": "Chen", "Age": "29", "Address": "Beijing", "Country": "China"},
-    {"Name": "Olga", "Age": "31", "Address": "Moscow", "Country": "Russia"},
-    {"Name": "Diego", "Age": "32", "Address": "Mexico City", "Country": "Mexico"},
-    {"Name": "Aisha", "Age": "21", "Address": "Cairo", "Country": "Egypt"},
-    {"Name": "Matteo", "Age": "33", "Address": "Rome", "Country": "Italy"},
-    {"Name": "Sofia", "Age": "26", "Address": "Athens", "Country": "Greece"},
-    {"Name": "Harun", "Age": "24", "Address": "Seoul", "Country": "South Korea"},
-    {"Name": "Zara", "Age": "22", "Address": "Dubai", "Country": "UAE"},
-    {"Name": "Isaac", "Age": "29", "Address": "Toronto", "Country": "Canada"},
-    {"Name": "Nina", "Age": "27", "Address": "Berlin", "Country": "Germany"},
-    {"Name": "Lucas", "Age": "28", "Address": "Sao Paulo", "Country": "Brazil"}
+  var products = [
+    {'title': 'Product 1', 'price': "\$19.99", 'image': 'https://cdn.pixabay.com/photo/2016/06/28/05/10/laptop-1483974_1280.jpg'},
+    {'title': 'Product 2', 'price': "\$29.99", 'image': 'https://cdn.pixabay.com/photo/2016/11/22/23/40/hands-1851218_1280.jpg'},
+    {'title': 'Product 3', 'price': "\$39.99", 'image': 'https://cdn.pixabay.com/photo/2016/10/10/14/17/refrigerator-1728501_1280.jpg'},
+    {'title': 'Product 4', 'price': "\$49.99", 'image': 'https://cdn.pixabay.com/photo/2019/06/30/18/19/tv-4308537_1280.jpg'},
+    {'title': 'Product 5', 'price': "\$59.99", 'image': 'https://cdn.pixabay.com/photo/2017/04/04/17/22/netgear-2202273_1280.jpg'},
+    {'title': 'Product 6', 'price': "\$69.99", 'image': 'https://cdn.pixabay.com/photo/2017/03/19/01/43/living-room-2155376_1280.jpg'},
+    {'title': 'Product 7', 'price': "\$79.99", 'image': 'https://cdn.pixabay.com/photo/2017/08/02/01/01/living-room-2569325_1280.jpg'},
+    {'title': 'Product 8', 'price': "\$89.99", 'image': 'https://cdn.pixabay.com/photo/2015/01/19/13/51/car-604019_1280.jpg'},
+    {'title': 'Product 9', 'price': "\$99.99", 'image': 'https://cdn.pixabay.com/photo/2016/11/18/21/30/bike-1836962_1280.jpg'},
+    {'title': 'Product 10','price': "\$109.99",'image': 'https://cdn.pixabay.com/photo/2018/06/12/19/59/football-3471371_1280.jpg'},
   ];
   @override
   Widget build(BuildContext context) {
@@ -47,19 +37,36 @@ class HomeScreen extends StatelessWidget{
         title: Text("Essential Widgets"),
       ),
       body: GridView.builder(
-          itemCount: myItems.length,
+          itemCount: products.length,
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
+              crossAxisSpacing: 2
           ),
           itemBuilder: (context,index){
             return InkWell(
               onTap: (){
-                mySnackBar(context, myItems[index]["Name"]);
+                mySnackBar(context, products[index]['title']);
               },
-              child: ListTile(
-                  leading: Icon(Icons.account_box_sharp),
-                  title: Text(myItems[index]["Name"]!),
-                  subtitle: Text(myItems[index]["Address"]!),
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                        child: Image.network(
+                          products[index]["image"]!,
+                          fit: BoxFit.fill,
+                        )
+                    ),
+                    Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Text(products[index]["title"]!),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text(products[index]["price"]!),
+                    )
+                  ],
+                ),
               ),
             );
           }),
