986abf54c5e0f40b517632dc29b0e8fb76643438
ashraf <ashrafhossainsamiu@gmail.com>
2023-12-26 20:21:29 +0600

    ListViewBuilder Commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,27 @@ class MyApp extends StatelessWidget{
   }
 }
 class HomeScreen extends StatelessWidget{
-  List<String> students= [
-    "Ashraf",
-    "Hossain",
-    "Rahim",
-    "Karim",
-    "Afsar",
-    "John",
-    "Price",
-    "Doe",
-    "Abrar",
-    "Jimmy"
+  var myItems = [
+    {"Name": "Ashraf", "Age": "20", "Address": "Dhaka City", "Country": "Bangladesh"},
+    {"Name": "John", "Age": "25", "Address": "New York", "Country": "USA"},
+    {"Name": "Emma", "Age": "22", "Address": "London", "Country": "UK"},
+    {"Name": "Carlos", "Age": "30", "Address": "Madrid", "Country": "Spain"},
+    {"Name": "Yuki", "Age": "28", "Address": "Tokyo", "Country": "Japan"},
+    {"Name": "Mia", "Age": "24", "Address": "Sydney", "Country": "Australia"},
+    {"Name": "Pablo", "Age": "26", "Address": "Buenos Aires", "Country": "Argentina"},
+    {"Name": "Lily", "Age": "23", "Address": "Paris", "Country": "France"},
+    {"Name": "Ravi", "Age": "27", "Address": "Mumbai", "Country": "India"},
+    {"Name": "Chen", "Age": "29", "Address": "Beijing", "Country": "China"},
+    {"Name": "Olga", "Age": "31", "Address": "Moscow", "Country": "Russia"},
+    {"Name": "Diego", "Age": "32", "Address": "Mexico City", "Country": "Mexico"},
+    {"Name": "Aisha", "Age": "21", "Address": "Cairo", "Country": "Egypt"},
+    {"Name": "Matteo", "Age": "33", "Address": "Rome", "Country": "Italy"},
+    {"Name": "Sofia", "Age": "26", "Address": "Athens", "Country": "Greece"},
+    {"Name": "Harun", "Age": "24", "Address": "Seoul", "Country": "South Korea"},
+    {"Name": "Zara", "Age": "22", "Address": "Dubai", "Country": "UAE"},
+    {"Name": "Isaac", "Age": "29", "Address": "Toronto", "Country": "Canada"},
+    {"Name": "Nina", "Age": "27", "Address": "Berlin", "Country": "Germany"},
+    {"Name": "Lucas", "Age": "28", "Address": "Sao Paulo", "Country": "Brazil"}
   ];
   @override
   Widget build(BuildContext context) {
@@ -31,29 +41,15 @@ class HomeScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text("Essential Widgets"),
       ),
-      body: Scrollbar(
-        thickness: 15,
-        radius: Radius.circular(15),
-        child: ListView.separated(
-            itemCount: students.length,
-            itemBuilder: (context,index){
-              return ListTile(
-                leading: Icon(Icons.star),
-                trailing: Icon(Icons.safety_check),
-                title: Text(students[index]),
-                subtitle: Text("StudentName"),
-              );
-            },
-            separatorBuilder: (context,index){
-              return Divider(
-                height: 24,
-                thickness: 5,
-                color: Colors.red,
-                indent: 16,
-                endIndent: 16,
-              );
-            }),
-      ),
+      body: ListView.builder(
+          itemCount: myItems.length,
+          itemBuilder: (context,index){
+            return ListTile(
+              leading: Icon(Icons.star),
+              title: Text(myItems[index]["Name"]!),
+              subtitle: Text(myItems[index]["Address"]!),
+            );
+          }),
     );
   }
 }
