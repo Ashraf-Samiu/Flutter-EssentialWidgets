30d59b24c3e6d6ebd110c14f35c858a042f5bd92
ashraf <ashrafhossainsamiu@gmail.com>
2023-12-28 03:39:07 +0600

    Named_Navigation Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -8,12 +7,16 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeRoute(),
+      initialRoute: '/',
+      routes: {
+        '/': (context)=>HomeActivity(),
+        '/Login': (context)=>LoginActivity(),
+        '/Profile': (context)=>ProfileActiviy()
+      },
     );
   }
 }
-///Route
-class HomeRoute extends StatelessWidget{
+class HomeActivity extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,103 +27,57 @@ class HomeRoute extends StatelessWidget{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Home Screen"),
             ElevatedButton(
                 onPressed: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: 
-                          (context) =>
-                              RouteSettings()
-                      )
-                  );
-                }, 
-                child: Text("Go to Settings")
-            ),
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context)=>OrderScreen())
-                  );
+                  Navigator.pushNamed(context, "/Login");
                 },
-                  child: Text("Go to OrderScreen")
-              )
-          ],
-        ),
-      ),
-    );
-  }
-}
-///Route
-class RouteSettings extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Settings Screen"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("settings Screen"),
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.pushAndRemoveUntil(
-                      context, MaterialPageRoute(
-                      builder: (context)=>HomeRoute()
-                  ), (route) => false);
-                  ///true define backstack button will remain after going to the desired route
-                },
-                child: Text("Go to Home Screen")
+                child: Text("Go to Login")
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-///Route
-class OrderScreen extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Order Screen"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Order Screen"),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context)=> RouteSettings()
-                    ));
+            ElevatedButton(onPressed: (){
+                Navigator.pushReplacementNamed(context, "/Login");
               },
-                child: Text("Go to Settings"),
-            ),
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.pop(context);
-                },
-                child: Text("Back to Home")
-            ),
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.of(context).pop();
-                },
-                child: Text("Go to SettingsScreen")
+                child: Text("go to login")
             )
           ],
         ),
       ),
     );
   }
+}
+class LoginActivity extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: (){
+                Navigator.pushReplacementNamed(context, "/Profile");
+            },
+            child: Text("Go to Profile")
+        ),
+      ),
+    );
+  }
+}
+class ProfileActiviy extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Profile Activity")
+          ],
+        ),
+      ),
+    );
+  }
+  
 }
