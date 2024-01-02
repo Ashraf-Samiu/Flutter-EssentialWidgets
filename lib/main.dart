faa9c39bbfedb320828a474008f48418bb2d36ae
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-03 02:14:02 +0600

    BottomSheetDialog_AlertDialog commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,38 +29,60 @@ class HomeScreen extends StatelessWidget{
                     isDismissible: false,
                     backgroundColor: Colors.yellow,
                     barrierColor: Colors.pink.shade300,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)
+                      )
+                    ),
                     context: context,
-                    builder: (_) {
-                      return Column(
-                        children: [
-                          Text('Message'),
-                          Text('Hello world'),
-                        ],
+                    builder: (BuildContext context) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Center(
+                                child: Text('BottomSheet Message!')
+                            ), Text('Hello world'),
+                          ],
+                        ),
                       );
                     });
               },child: Text('Tap here'),
             ),
             ElevatedButton(onPressed: (){
-                showAboutDialog(context: context);
+                showAboutDialog(
+                  context: context,
+                  applicationName: "Ashraf's Software",
+                  applicationVersion: "3.03",
+                  applicationIcon: Icon(Icons.account_box),
+                  children: [
+                    Text("A Practice Application")
+                  ]
+                );
               }, child: Text("Tap")
             ),
             ElevatedButton(
                 onPressed: (){
-                    showDialog(context: context, builder: (context) {
-                     return AlertDialog(
-                        title: Text('Message'),
-                        content: Text('Hello, welcome to app'),
-                        actions: [
-                         ElevatedButton(onPressed: () {},
-                           child: Text('cancel'),
-                         ),
-                      ],
-                     );
-                   });
-                }, child: Text("Click Here!"))
-          ],
+                    showDialog(
+                      barrierDismissible: false,
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text('Message'),
+                            content: Text('Hello, welcome to app'),
+                            actions: [
+                              ElevatedButton(onPressed: () {},
+                                child: Text('cancel'),
+                            ),
+                          ],
+                        );
+                      });
+                    }, child: Text("Click Here!")
+                  )
+              ],
+          ),
         ),
-      ),
     );
   }
 
