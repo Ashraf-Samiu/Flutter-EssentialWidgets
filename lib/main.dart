eac022fe70497ad03c0e335f8a36838f0a980525
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-03 02:04:44 +0600

    ProductList_with_Navigation commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,11 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeRoute(),
+      home: HomeScreen(),
     );
   }
 }
-///Route
-class HomeRoute extends StatelessWidget{
+class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,112 +22,173 @@ class HomeRoute extends StatelessWidget{
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Home Screen"),
-            ElevatedButton(
-                onPressed: (){
-                  /// Route = Screen
-                  /// Navigator - 1,2
-                  /// Route 1(Current screen) -> Route 2 (Route Settings)
-                  /// Step 1- Navigator - push
-                  /// Step 2 - Context (current route)
-                  /// Step 3 - Convert RouteSettings as Route with MaterialPageRoute
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => RouteSettings()
-                      )
-                  );
-                }, child: Text("Go to Settings")
+            Text(
+              "Home",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w800
+              ),
             ),
             ElevatedButton(
                 onPressed: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context)=>OrderScreen())
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context)=>SettingsScreen()
+                    )
                   );
-                },
-                child: Text("Go to OrderScreen")
-            )
+                }, child: Text("Go to the settings screen")),
+            ElevatedButton(
+                onPressed: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context)=>OrderScreen()
+                    )
+                  );
+                }, child: Text("Go to order screen")),
+            ElevatedButton(
+                onPressed: (){
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context)=>ProductListScreen()
+                    )
+                  );
+              }, child: Text("Go to product Screen"))
           ],
         ),
       ),
     );
   }
 }
-///Route
-class RouteSettings extends StatelessWidget{
+class SettingsScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Settings Screen"),
+        title: Text("Settings"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("settings Screen"),
+            Text(
+              "Settings",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w800
+              ),
+            ),
             ElevatedButton(
                 onPressed: (){
-                  /// Back to previous screen
-                  Navigator.pushAndRemoveUntil(
-                      context, MaterialPageRoute(
-                      builder: (context)=>HomeRoute()
-                  ), (route) => false);
-                  ///true define backstack button will remain after going to the desired route
-                },
-                child: Text("Go to Home Screen")
-            ),
+                Navigator.pushAndRemoveUntil(
+                    context, MaterialPageRoute(
+                    builder: (context)=> HomeScreen()
+                ), (route) => false);
+              },child: Text("Go to home")),
           ],
         ),
       ),
     );
   }
 }
-///Route
 class OrderScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Order Screen"),
+        title: Text("Orders"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Order Screen"),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.pushReplacement(
+            Text(
+              "Orders",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w800
+              ),
+            ),
+            TextButton(
+                onPressed: (){
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(
+                      builder: (context)=>SettingsScreen()
+                  )
+                );
+              },child: Text("Go to settings")),
+            TextButton(
+                onPressed: (){
+                  Navigator.pop(context);
+            }, child: Text("Back to Home")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class ProductListScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Product List"),
+      ),
+      body: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (context,index){
+            return ListTile(
+              onTap: (){
+                Navigator.push(
                     context,
                     MaterialPageRoute(
-                        builder: (context)=> RouteSettings()
-                    ));
-              },
-              child: Text("'Go to Settings by replace'"),
+                    builder: (context){
+                      return ProductDetailsScreen(
+                          productName: index.toString(),
+                          price: 1245);
+                      //short form of return is =>
+                    })).then((value){
+                      print(value);
+                    });
+                  },
+              title: Text(index.toString()),
+              subtitle: Text("product details $index"),
+            );
+          }),
+    );
+  }
+}
+class ProductDetailsScreen extends StatelessWidget{
+  final String productName;
+  final double? price;
+  const ProductDetailsScreen({super.key,
+    required this.productName,
+    this.price });
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Product Details"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              productName,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w700
+              ),
             ),
             ElevatedButton(
                 onPressed: (){
-                  /// Back to previous screen
-                  Navigator.pop(context);
-                },
-                child: Text("Back to Home")
-            ),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.of(context).pop();
-              },
-              child: Text("Go to previous screen"),
-            ),
-            ElevatedButton(onPressed: (){
-              /// Back to previous screen (will skip the previous screen)
-              Navigator.pushAndRemoveUntil(
-                  context, MaterialPageRoute(
-                  builder: (context) => HomeRoute()
-              ), (route) => false);
-            }, child: Text("Go to Home"))
+                Navigator.pop
+                  (context,"productList No-$productName");
+              },child: Text("Back")
+            )
           ],
         ),
       ),
