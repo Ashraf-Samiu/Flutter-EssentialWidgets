fd2f69576c5cb0bd1f0963fc1f11b4641a41a595
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-03 01:53:56 +0600

    DataPassing_with_Navigation commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,25 +19,50 @@ class HomeScreen extends StatelessWidget{
       appBar: AppBar(
         title: Text("Home"),
       ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){
+              Navigator.push(
+                  context,
+                  MaterialPageRoute
+                    (builder: (context)=>SettingsScreen(userName: "Ashraf")
+                  )
+                ).then((value) {
+                  print(value); ///Part of Async Programming
+                });
+              }, child: Text("Go to Settings")
+            )
+          ],
+        ),
+      ),
     );
   }
 }
 class SettingsScreen extends StatelessWidget{
+  final String userName;
+  final int? age;
+  SettingsScreen({required this.userName,this.age});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Settings"),
       ),
-    );
-  }
-}
-class OrderScreen extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Order"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(userName),
+            Text(age.toString()),
+            ElevatedButton(onPressed: (){
+              Navigator.pop(context,{
+                "Karim": 21
+              });
+            }, child: Text("Back to Home"))
+          ],
+        ),
       ),
     );
   }
