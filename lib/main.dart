e4a8612f44b06b102944fec2cc0cb0548ca72a41
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-03 01:37:59 +0600

    DataPassing_with_Navigation commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,133 +7,37 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeRoute(),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
-///Route
-class HomeRoute extends StatelessWidget{
+class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Home Screen"),
-            ElevatedButton(
-                onPressed: (){
-                  /// Route = Screen
-                  /// Navigator - 1,2
-                  /// Route 1(Current screen) -> Route 2 (Route Settings)
-                  /// Step 1- Navigator - push
-                  /// Step 2 - Context (current route)
-                  /// Step 3 - Convert RouteSettings as Route with MaterialPageRoute
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder:
-                              (context) =>
-                              RouteSettings()
-                      )
-                  );
-                },
-                child: Text("Go to Settings")
-            ),
-            ElevatedButton(
-                onPressed: (){
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context)=>OrderScreen())
-                  );
-                },
-                child: Text("Go to OrderScreen")
-            )
-          ],
-        ),
-      ),
     );
   }
 }
-///Route
-class RouteSettings extends StatelessWidget{
+class SettingsScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Settings Screen"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("settings Screen"),
-            ElevatedButton(
-                onPressed: (){
-                  /// Back to previous screen
-                  Navigator.pushAndRemoveUntil(
-                      context, MaterialPageRoute(
-                      builder: (context)=>HomeRoute()
-                  ), (route) => false);
-                  ///true define backstack button will remain after going to the desired route
-                },
-                child: Text("Go to Home Screen")
-            ),
-          ],
-        ),
+        title: Text("Settings"),
       ),
     );
   }
 }
-///Route
 class OrderScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Order Screen"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Order Screen"),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context)=> RouteSettings()
-                    ));
-              },
-              child: Text("Go to Settings"),
-            ),
-            ElevatedButton(
-                onPressed: (){
-                  /// Back to previous screen
-                  Navigator.pop(context);
-                },
-                child: Text("Back to Home")
-            ),
-            ElevatedButton(
-              onPressed: (){
-                Navigator.of(context).pop();
-              },
-              child: Text("Go to previous screen"),
-            ),
-            ElevatedButton(onPressed: (){
-              /// Back to previous screen (will skip the previous screen)
-              Navigator.pushAndRemoveUntil(
-                  context, MaterialPageRoute(
-                  builder: (context) => HomeRoute()
-              ), (route) => false);
-            }, child: Text("Go to Home"))
-          ],
-        ),
+        title: Text("Order"),
       ),
     );
   }
