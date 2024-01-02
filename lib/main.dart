d88b0785c4877adc1fc7a5502ab8c731b3f8fda7
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-03 01:33:44 +0600

    MaterialPageRoute_Navigation commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,25 +7,134 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeActivity(),
+      home: HomeRoute(),
     );
   }
 }
-class HomeActivity extends StatelessWidget{
+///Route
+class HomeRoute extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Code"),
+        title: Text("Home"),
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("data")
+            Text("Home Screen"),
+            ElevatedButton(
+                onPressed: (){
+                  /// Route = Screen
+                  /// Navigator - 1,2
+                  /// Route 1(Current screen) -> Route 2 (Route Settings)
+                  /// Step 1- Navigator - push
+                  /// Step 2 - Context (current route)
+                  /// Step 3 - Convert RouteSettings as Route with MaterialPageRoute
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder:
+                              (context) =>
+                              RouteSettings()
+                      )
+                  );
+                },
+                child: Text("Go to Settings")
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context)=>OrderScreen())
+                  );
+                },
+                child: Text("Go to OrderScreen")
+            )
           ],
-        )
+        ),
       ),
     );
   }
 }
-
+///Route
+class RouteSettings extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Settings Screen"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("settings Screen"),
+            ElevatedButton(
+                onPressed: (){
+                  /// Back to previous screen
+                  Navigator.pushAndRemoveUntil(
+                      context, MaterialPageRoute(
+                      builder: (context)=>HomeRoute()
+                  ), (route) => false);
+                  ///true define backstack button will remain after going to the desired route
+                },
+                child: Text("Go to Home Screen")
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+///Route
+class OrderScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Order Screen"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Order Screen"),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context)=> RouteSettings()
+                    ));
+              },
+              child: Text("Go to Settings"),
+            ),
+            ElevatedButton(
+                onPressed: (){
+                  /// Back to previous screen
+                  Navigator.pop(context);
+                },
+                child: Text("Back to Home")
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+              },
+              child: Text("Go to previous screen"),
+            ),
+            ElevatedButton(onPressed: (){
+              /// Back to previous screen (will skip the previous screen)
+              Navigator.pushAndRemoveUntil(
+                  context, MaterialPageRoute(
+                  builder: (context) => HomeRoute()
+              ), (route) => false);
+            }, child: Text("Go to Home"))
+          ],
+        ),
+      ),
+    );
+  }
+}
