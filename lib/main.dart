beb97a114f328b3b032354922cb426277df82392
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-04 15:10:47 +0600

    CupertinoApp commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -6,90 +7,9 @@ void main(){
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.redAccent,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.greenAccent,
-            foregroundColor: Colors.yellow,
-            padding: EdgeInsets.symmetric(
-              horizontal: 16,vertical: 16
-            ),
-            elevation: 10,
-            textStyle: TextStyle(
-              fontSize: 15,
-              fontWeight: FontWeight.w600,
-              letterSpacing: 0.8,
-              wordSpacing: 0.6
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(0)
-            ),
-          ),
-        ),
-        textButtonTheme: TextButtonThemeData(
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.white,
-            textStyle: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.w500
-            )
-          )
-        ),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.purple,
-          elevation: 5,
-          shadowColor: Colors.blueAccent,
-          titleTextStyle: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
-            color: Colors.yellow
-          )
-        ),
-        textTheme: TextTheme(
-          bodyMedium: TextStyle(
-            fontSize: 18, ///by default value 14...
-          ),
-          bodyLarge: TextStyle(
-            fontSize: 22
-          ),
-          bodySmall: TextStyle(
-            fontSize: 10
-          ),
-          ///we can take both body small and large by calling the themeData
-        ),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.white
-        ),
-      ),
-      ///we can use only one if we use light the theme property will work
-      ///but if we use dark the darkTheme property will work
-      themeMode: ThemeMode.light,
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.purpleAccent,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            foregroundColor: Colors.purpleAccent,
-            backgroundColor: Colors.greenAccent,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),
-              side: BorderSide(
-                width: 2,
-                color: Colors.red
-              ),
-            ),
-          )
-        ),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.brown,
-          elevation: 10,
-          shadowColor: Color(0xFF800000)
-        )
-      ),
+    ///MaterialPackage components are usable in CupertinoApp
+    ///CupertinoApp is totally IOS system-based Design..
+    return CupertinoApp(
       home: HomeScreen(),
     );
   }
@@ -97,52 +17,27 @@ class MyApp extends StatelessWidget{
 class HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Home",),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: (){},
-          child: Icon(Icons.account_box),
-      ),
-      body: Center(
+    return CupertinoPageScaffold(
+        ///In Ios There is no such things as appBar..the things that exist is NavigationBar
+        navigationBar: CupertinoNavigationBar(
+          middle: Text("Home"),
+          backgroundColor: Colors.grey,
+          leading: Icon(CupertinoIcons.home, size: 18),
+          trailing: Icon(CupertinoIcons.ant_circle),
+        ),
         child: Column(
           children: [
-            Text("This World is very Beautiful"),
-            Text("Hello World!",
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            Text("Hello World!",
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-            Text("Hello World!",
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-            Text("Hello World!",
-              style: Theme.of(context).textTheme.headlineLarge,
-            ),
-            Text("Hello World!",
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text("Hello World!",
-              style: Theme.of(context).textTheme.headlineSmall,
-            ),
-            ElevatedButton(onPressed: (){},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.purpleAccent,
-                  foregroundColor: Colors.greenAccent
-                ), child: Text("data")),
-            ElevatedButton(onPressed: (){}, child: Text("Information")),
-            ElevatedButton(onPressed: (){}, child: Text("Documentation")),
-            TextButton(onPressed: (){},
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.indigo
-                ), child: Text("Dart")),
-            TextButton(onPressed: (){}, child: Text("Flutter")),
+            CupertinoButton(child: Text("Tap Here"), onPressed: (){}),
+            CupertinoButton.filled(child: Text("Click Here"), onPressed: (){}),
+            ElevatedButton(onPressed: (){}, child: Text("Click")),
+            CupertinoTabBar(items: [
+              BottomNavigationBarItem(icon: Icon(CupertinoIcons.home)),
+              BottomNavigationBarItem(icon: Icon(CupertinoIcons.settings)),
+              BottomNavigationBarItem(icon: Icon(CupertinoIcons.person))
+            ]),
+            CupertinoTextField(),
           ],
         ),
-      ),
-    );
+     );
   }
 }
