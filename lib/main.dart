ddfa5e41257d7cfb9b14a882cdb8c7c2c92cb5a9
ashraf <ashrafhossainsamiu@gmail.com>
2024-01-04 14:46:17 +0600

    ThemeData commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,85 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.redAccent,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.greenAccent,
+            foregroundColor: Colors.yellow,
+            padding: EdgeInsets.symmetric(
+              horizontal: 16,vertical: 16
+            ),
+            elevation: 10,
+            textStyle: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 0.8,
+              wordSpacing: 0.6
+            ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(0)
+            ),
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            textStyle: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500
+            )
+          )
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.purple,
+          elevation: 5,
+          shadowColor: Colors.blueAccent,
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+            color: Colors.yellow
+          )
+        ),
+        textTheme: TextTheme(
+          bodyMedium: TextStyle(
+            fontSize: 18, ///by default value 14...
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 22
+          ),
+          bodySmall: TextStyle(
+            fontSize: 10
+          ),
+          ///we can take both body small and large by calling the themeData
+        )
+      ),
+      ///we can use only one if we use light the theme property will work
+      ///but if we use dark the darkTheme property will work
+      themeMode: ThemeMode.light,
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.purpleAccent,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.purpleAccent,
+            backgroundColor: Colors.greenAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+              side: BorderSide(
+                width: 2,
+                color: Colors.red
+              ),
+            ),
+          )
+        ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.brown,
+          elevation: 10,
+          shadowColor: Color(0xFF800000)
+        )
+      ),
       home: HomeScreen(),
     );
   }
@@ -23,67 +101,40 @@ class HomeScreen extends StatelessWidget{
       body: Center(
         child: Column(
           children: [
-            ElevatedButton(
-              onPressed: () {
-                showModalBottomSheet(
-                    isDismissible: false,
-                    backgroundColor: Colors.yellow,
-                    barrierColor: Colors.pink.shade300,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(20),
-                        topRight: Radius.circular(20)
-                      )
-                    ),
-                    context: context,
-                    builder: (BuildContext context) {
-                      return Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
-                          children: [
-                            Center(
-                                child: Text('BottomSheet Message!')
-                            ), Text('Hello world'),
-                          ],
-                        ),
-                      );
-                    });
-              },child: Text('Tap here'),
+            Text("This World is very Beautiful"),
+            Text("Hello World!",
+              style: Theme.of(context).textTheme.bodyLarge,
             ),
-            ElevatedButton(onPressed: (){
-                showAboutDialog(
-                  context: context,
-                  applicationName: "Ashraf's Software",
-                  applicationVersion: "3.03",
-                  applicationIcon: Icon(Icons.account_box),
-                  children: [
-                    Text("A Practice Application")
-                  ]
-                );
-              }, child: Text("Tap")
+            Text("Hello World!",
+              style: Theme.of(context).textTheme.bodySmall,
             ),
-            ElevatedButton(
-                onPressed: (){
-                    showDialog(
-                      barrierDismissible: false,
-                        context: context,
-                        builder: (context) {
-                          return AlertDialog(
-                            title: Text('Message'),
-                            content: Text('Hello, welcome to app'),
-                            actions: [
-                              ElevatedButton(onPressed: () {},
-                                child: Text('cancel'),
-                            ),
-                          ],
-                        );
-                      });
-                    }, child: Text("Click Here!")
-                  )
-              ],
-          ),
+            Text("Hello World!",
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            Text("Hello World!",
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            Text("Hello World!",
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Text("Hello World!",
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+            ElevatedButton(onPressed: (){},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purpleAccent,
+                  foregroundColor: Colors.greenAccent
+                ), child: Text("data")),
+            ElevatedButton(onPressed: (){}, child: Text("Information")),
+            ElevatedButton(onPressed: (){}, child: Text("Documentation")),
+            TextButton(onPressed: (){},
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.indigo
+                ), child: Text("Dart")),
+            TextButton(onPressed: (){}, child: Text("Flutter")),
+          ],
         ),
+      ),
     );
   }
-
 }
